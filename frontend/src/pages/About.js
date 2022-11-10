31a7d3a95aad081eae700134c8327e0ce436313e
Dongjoo Kim <50216530+mosmomedia@users.noreply.github.com>
2022-11-10 23:29:51 +0900

    feat: about page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -3,7 +3,16 @@ import Component from '../core/Component.js';
 export default class About extends Component {
 	template() {
 		return `
-		<div>About</div>
+			<div id='about' class='card-header'>
+				<div>
+					<h1 >About This Project</h1>
+					<p>This is a React app to leave feedback for a product or service</p>
+					<p>
+						This is the project based from one of bradtraversy's udemy courses,
+						"React Front To Back 2022".
+					</p>
+					<p>Version: 0.1.0</p>
+			</div>
 		`;
 	}
 }
